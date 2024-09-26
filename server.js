df2c911236fffa81f25533ae20226a5f1f6ff17d
parkyooni <elmi1003@naver.com>
2024-09-27 07:09:29 +0900

    Feat: CORS

--- server.js
+++ server.js
@@ -9,19 +9,23 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(
   cors({
     origin: "*",
-    credentials: true,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
   })
 );
+app.options("*", cors());
 
 app.use(express.json());
 
 app.use("/api/layers", layerRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
